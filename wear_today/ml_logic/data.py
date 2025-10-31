1247434111720a28bfb49daa2792a19c944b8953
Benjamin Simeoni <benjamin.simeoni@gmail.com>
2025-10-31 18:12:59 +0100

    feat(clean_data) : clean_data and fill_missing_timestamps added to data.py

--- wear_today/ml_logic/data.py
+++ wear_today/ml_logic/data.py
@@ -7,6 +7,48 @@ from retry_requests import retry
 from .utils import get_coords_from_location_name, get_timezone_from_coords
 from .params import *
 
+
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean raw data by
+    - filling missing data with interpolated data
+    """
+
+    #Interpolate missing data
+    df = fill_missing_timestamps(df)
+
+    print("✅ data cleaned")
+
+    return df
+
+
+def fill_missing_timestamps(df:pd.DataFrame)->pd.DataFrame:
+    '''
+    Check if some rows are missing; fill them with linear interpolation and return
+    the full dataframe.
+    '''
+    #Set date to index to build a date range with full datetime
+    df.set_index('date',inplace = True, drop = True)
+    full_index = pd.date_range(start=df.index.min(),
+                           end=df.index.max(),
+                           freq='h')
+
+    #Detect the missing timestamps
+    missing_timestamps = full_index.difference(df.index)
+
+    #Define new indices for our df with the whol list of indices and
+    #interpolate missing values
+    df = df.reindex(full_index)
+    df= df.interpolate(method='linear')
+    df.index.name = 'date'
+    if len(missing_timestamps) != 0:
+        print(f"{len(missing_timestamps)} missing timestamp(s) interpolated")
+    #Return the cleaned dataframe and put back 'date' as a column
+    return df.reset_index().rename(columns={'index': 'date'})
+
+
+
 def get_meteo_data_with_cache(start_date: str = "2023-01-01",
                         end_date: str = "2025-10-15",
                         location: str = "Berlin, Germany"):
