2c417b724f126c47b91a28852c10386b498ee70b
Benjamin Simeoni <benjamin.simeoni@gmail.com>
2025-11-04 13:24:25 +0100

    fix(recommend_clothes_for_today): adapt code to use weather prediction

--- wear_today/interface/main.py
+++ wear_today/interface/main.py
@@ -14,19 +14,11 @@ def recommend_clothes_for_today(city="Berlin, Germany", today=datetime.now()):
     # receive 12 hour weather prediction
     # obtain clothing recommendation
     print(f"Gathering clothing recommendation for {city}")
-    weather_predictor = WeatherPredictor()
+    weather_predictor = WeatherPredictor(location = city, today = today)
     clothing_recommender = TemperatureRecommender()
     clothing_recommender.load_data()
-    weather_prediction = weather_predictor.predict(today)
-    input = {
-        "time": [datetime.now() + timedelta(hours=i) for i in range(6)],
-        "humidity": [60, 50, 50, 50, 60, 70],
-        "temperature": [11, 11, 12, 12, 13, 13],
-        "wind": [15, 16, 18, 20, 22, 30],
-        "rain": [0, 0, 0, 0, 1.0, 1.5],
-    }
-    df = pd.DataFrame(input)  # replace with weather_prediction
-    recommended_clothing = clothing_recommender.recommend(df, top_k=1, sample_size=10)
+    weather_prediction = weather_predictor.predict()
+    recommended_clothing = clothing_recommender.recommend(weather_prediction, top_k=1, sample_size=10)
 
     return weather_prediction, recommended_clothing
 
