a6b5dc2d28e6f2782618cdc22f08706a0861f2f1
Stefan Seelig <90114022+stfnlxndrslg@users.noreply.github.com>
2025-11-05 13:52:10 +0100

    feature: save/load clothes embeddings; fix: removed product_id from results, because it was not in the updated CSV files

--- wear_today/ml_logic/recommender_model.py
+++ wear_today/ml_logic/recommender_model.py
@@ -20,7 +20,7 @@ class ClothingRecommender:
 
         current_file = Path(__file__).resolve()
         root_dir = current_file.parent.parent.parent
-        self.cache_dir= os.path.join(root_dir, MODELS_DIRECTORY)
+        self.cache_dir = os.path.join(root_dir, MODELS_DIRECTORY)
         os.makedirs(self.cache_dir, exist_ok=True)
 
     def load_data(self):
@@ -44,6 +44,23 @@ class ClothingRecommender:
         self.df_clothes = pd.concat((df_accessories, df_shoes, df_tops, df_bottoms))
 
         print(f"✅ Loaded {len(self.df_clothes)} clothing items")
+        '''if embeddings file exists load embeddings,
+        otherwise save embeddings'''
+
+        self.clothes_embeddings = {}
+        for clothes_cat in self.df_clothes['category_type'].unique():
+            file_path = os.path.join(encoding_data_path, f"embeddings_{clothes_cat}.npy")
+            if os.path.exists(file_path):
+                print(f"✅ Loading wardrobe embeddings for {clothes_cat}!")
+                self.clothes_embeddings[clothes_cat] = np.load(file_path)
+            else:
+                print(f"✅ Embedding wardrobe for {clothes_cat}!")
+                mask = list(self.df_clothes['category_type'] == clothes_cat)
+                self.clothes_embeddings[clothes_cat] = self.embedder.encode(
+                    self.df_clothes.iloc[mask]['text_complex']
+                    )
+                np.save(file_path, self.clothes_embeddings[clothes_cat])
+
         return self
 
     def initialize_clothesmodel(self):
@@ -115,9 +132,9 @@ class ClothingRecommender:
         """
         # Single batch classification (fast!)
         print("⚡ Classifying sampled items in one batch...")
-        clothing_emb = self.embedder.encode(input["clothes"])
+        # clothing_emb = self.embedder.encode(input["clothes"])
         weather_emb = self.embedder.encode(input['weather'])
-        scores = cosine_similarity([weather_emb], clothing_emb)[0]
+        scores = cosine_similarity([weather_emb], input['clothes_emb'])[0]
         return scores
 
     def recommend(self, df_weather, top_k=5, sample_size=200):
@@ -148,24 +165,22 @@ class ClothingRecommender:
             wardrobe = self.df_clothes[mask]
             # RANDOM SAMPLE - This is the key speed improvement!
             if len(wardrobe) > sample_size:
-                sample_df = wardrobe.sample(sample_size)  # , random_state=42)
+                sample_idx = np.random.choice(wardrobe.index, size=sample_size, replace=False)
                 print(
                     f"🎯 Sampling {sample_size} random items from {len(wardrobe)} total items"
                 )
             else:
-                sample_df = wardrobe
-                print(f"🎯 Using all {len(sample_df)} items")
+                sample_idx = np.random.choice(wardrobe.index, size=len(wardrobe), replace=False)
+                print(f"🎯 Using all {len(wardrobe)} items")
 
-            # Prepare texts from sampled items
-            clothing_info = []
-            for idx, row in sample_df.iterrows():
-                label = row['text_complex']
-                clothing_info.append(label)
+            # select corresponding rows from embeddings
+            sample_df = wardrobe.iloc[sample_idx]
+            clothes_emb_sample = self.clothes_embeddings[clo_cat][sample_idx,:]
 
             # Single batch classification (fast!)
             input = {
                 "weather": weather_sentence,  # TODO: we currently have a sentence for each of the 12 hours of forecast, but only feed 1 to the model
-                "clothes": clothing_info,
+                "clothes_emb": clothes_emb_sample,
             }
 
             scores = self.call_embedder(input)
@@ -180,7 +195,6 @@ class ClothingRecommender:
                         "category": clo_cat,
                         "rank": i + 1,
                         "product_name": item["product_name"],
-                        "product_id": item["product_id"],
                         "gender": item["gender"],
                         "details": item["details"],
                         "images": item["product_images"],
